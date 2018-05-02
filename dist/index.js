691fca35e1a74e9c34c7a47c40d102fe7a23dcb1
Makar Stetsenko <makazone@gmail.com>
2018-05-02 19:18:20 +0300

    Change run script

--- dist/index.js
+++ dist/index.js
@@ -3,31 +3,131 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("core-js/shim");
 const graphql_1 = require("graphql");
+const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const lodash_1 = require("lodash");
 const proxy_1 = require("./proxy");
 const fake_definition_graphql_1 = require("./fake_definition_graphql");
+const app = express();
 function buildServerSchema(idl) {
-    const fakeIDL = new graphql_1.Source(fake_definition_graphql_1.default, "Fake definition AST");
-    const fakeAST = graphql_1.parse(fakeIDL);
-    var ast = graphql_1.concatAST([graphql_1.parse(idl), fakeAST]);
-    return graphql_1.buildASTSchema(ast);
+  const fakeIDL = new graphql_1.Source(
+    fake_definition_graphql_1.default,
+    "Fake definition AST"
+  );
+  const fakeAST = graphql_1.parse(fakeIDL);
+  var ast = graphql_1.concatAST([graphql_1.parse(idl), fakeAST]);
+  return graphql_1.buildASTSchema(ast);
 }
-exports.fakerProxy = (fakedSchema, url) => graphqlHTTP(req => {
-    const fakeIDL = new graphql_1.Source(fakedSchema, `Introspection from faked schema`);
-    return proxy_1.proxyMiddleware(url, [])
-        .then(([schemaIDL, cb]) => {
-        schemaIDL = new graphql_1.Source(schemaIDL, `Inrospection from "${url}"`);
+const fakerProxy = (fakedSchema, url) =>
+  graphqlHTTP(req => {
+    const fakeIDL = new graphql_1.Source(
+      fakedSchema,
+      `Introspection from faked schema`
+    );
+    return proxy_1
+      .proxyMiddleware(url, [])
+      .then(([schemaIDL, cb]) => {
+        schemaIDL = new graphql_1.Source(
+          schemaIDL,
+          `Inrospection from "${url}"`
+        );
         if (fakeIDL) {
-            const schema = buildServerSchema(schemaIDL);
-            fakeIDL.body = fakeIDL.body.replace("<RootTypeName>", schema.getQueryType().name);
+          const schema = buildServerSchema(schemaIDL);
+          fakeIDL.body = fakeIDL.body.replace(
+            "<RootTypeName>",
+            schema.getQueryType().name
+          );
         }
         const schema = buildServerSchema(schemaIDL);
         const forwardHeaders = lodash_1.pick(req.headers, []);
-        return Object.assign({}, cb(schema, fakeIDL, forwardHeaders), { graphiql: true });
-    })
-        .catch(error => {
+        return Object.assign({}, cb(schema, fakeIDL, forwardHeaders), {
+          graphiql: true
+        });
+      })
+      .catch(error => {
         console.log(error);
-    });
-});
+      });
+  });
+const url =
+  "https://59y35kj8hj.execute-api.us-east-1.amazonaws.com/develop/graphql";
+const fakedSchema = `
+  extend type <RootTypeName> {
+    assets: [Asset]
+    symbols: [Symbol]
+    exchanges: [Exchange]
+    exchange(id: ID!): Exchange
+    trades(symbol: ID!): [Trade]
+    ticker(symbol: ID!): Ticker
+    orderBook: OrderBook
+    candlestick(from: String, to: String): Candlestick
+  }
+
+  type Asset {
+    name: String @examples(values: ["Bitcoin", "EOS", "Ethereum"])
+    symbol: String @examples(values: ["BTC", "EOS", "ETH"])
+  }
+
+  type Symbol {
+    base: Asset!
+    quote: Asset!
+  }
+
+  type Exchange {
+      name: String @examples(values: ["Huobi", "Bitfinex", "Binance", "Poloniex", "Bittrex"])
+      url: String @fake(type: url)
+  }
+
+  enum TradeType {
+      BUY
+      SELL
+  }
+
+  type Trade {
+    timestamp: String! @fake(type: recentDate, options: {dateFormat: "YYYY-MM-DD"})
+    symbol: Symbol!
+    price: Float @fake(type: money, options: {minMoney: 10, maxMoney: 1000, decimalPlaces: 2})
+    amount: Int
+    exchange: Exchange!
+    tradeType: TradeType
+  }
+
+  type Ticker {
+    symbol: Symbol!
+    timestamp: String! @fake(type: recentDate, options: {dateFormat: "YYYY-MM-DD"})
+    mid: Float!
+    bid: Float!
+    ask: Float!
+    lastPrice: Float @fake(type: money, options: {minMoney: 10, maxMoney: 1000, decimalPlaces: 2})
+    highestPrice: Float @fake(type: money, options: {minMoney: 10, maxMoney: 1000, decimalPlaces: 2})
+    lowestPrice: Float @fake(type: money, options: {minMoney: 10, maxMoney: 1000, decimalPlaces: 2})
+    volume: Float
+  }
+
+  type Order {
+      price: Float!
+      amount: Float!
+  }
+
+  type OrderBook {
+      symbol: Symbol
+      bids: [Order]
+      asks: [Order]
+      timestamp: String! @fake(type: recentDate, options: {dateFormat: "YYYY-MM-DD"})
+  }
+
+  type DataPoint {
+    timestamp: String! @fake(type: recentDate, options: {dateFormat: "YYYY-MM-DD"})
+    open: Float!
+    close: Float!
+    high: Float!
+    low: Float!
+    volume: Float!
+  }
+
+  type Candlestick {
+    dataPoints: [DataPoint!]!
+  }
+`;
+app.use("/graphql", fakerProxy(fakedSchema, url));
+app.listen(3001);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ import "core-js/shim";
 
 import { Source, parse, concatAST, buildASTSchema } from "graphql";
 
+const express = require("express");
 import * as graphqlHTTP from "express-graphql";
 import { pick } from "lodash";
 import { proxyMiddleware } from "./proxy";
 import fake_definition from "./fake_definition_graphql";
+
+const app = express();
 
 function buildServerSchema(idl) {
   const fakeIDL = new Source(fake_definition, "Fake definition AST");
@@ -16,7 +19,7 @@ function buildServerSchema(idl) {
   return buildASTSchema(ast);
 }
 
-export const fakerProxy = (fakedSchema, url) =>
+const fakerProxy = (fakedSchema, url) =>
   graphqlHTTP(req => {
     const fakeIDL = new Source(fakedSchema, `Introspection from faked schema`);
 
@@ -43,3 +46,89 @@ export const fakerProxy = (fakedSchema, url) =>
         console.log(error);
       });
   });
+
+const url =
+  "https://59y35kj8hj.execute-api.us-east-1.amazonaws.com/develop/graphql";
+
+const fakedSchema = `
+  extend type <RootTypeName> {
+    assets: [Asset]
+    symbols: [Symbol]
+    exchanges: [Exchange]
+    exchange(id: ID!): Exchange
+    trades(symbol: ID!): [Trade]
+    ticker(symbol: ID!): Ticker
+    orderBook: OrderBook
+    candlestick(from: String, to: String): Candlestick
+  }
+
+  type Asset {
+    name: String @examples(values: ["Bitcoin", "EOS", "Ethereum"])
+    symbol: String @examples(values: ["BTC", "EOS", "ETH"])
+  }
+
+  type Symbol {
+    base: Asset!
+    quote: Asset!
+  }
+
+  type Exchange {
+      name: String @examples(values: ["Huobi", "Bitfinex", "Binance", "Poloniex", "Bittrex"])
+      url: String @fake(type: url)
+  }
+
+  enum TradeType {
+      BUY
+      SELL
+  }
+
+  type Trade {
+    timestamp: String! @fake(type: recentDate, options: {dateFormat: "YYYY-MM-DD"})
+    symbol: Symbol!
+    price: Float @fake(type: money, options: {minMoney: 10, maxMoney: 1000, decimalPlaces: 2})
+    amount: Int
+    exchange: Exchange!
+    tradeType: TradeType
+  }
+
+  type Ticker {
+    symbol: Symbol!
+    timestamp: String! @fake(type: recentDate, options: {dateFormat: "YYYY-MM-DD"})
+    mid: Float!
+    bid: Float!
+    ask: Float!
+    lastPrice: Float @fake(type: money, options: {minMoney: 10, maxMoney: 1000, decimalPlaces: 2})
+    highestPrice: Float @fake(type: money, options: {minMoney: 10, maxMoney: 1000, decimalPlaces: 2})
+    lowestPrice: Float @fake(type: money, options: {minMoney: 10, maxMoney: 1000, decimalPlaces: 2})
+    volume: Float
+  }
+
+  type Order {
+      price: Float!
+      amount: Float!
+  }
+
+  type OrderBook {
+      symbol: Symbol
+      bids: [Order]
+      asks: [Order]
+      timestamp: String! @fake(type: recentDate, options: {dateFormat: "YYYY-MM-DD"})
+  }
+
+  type DataPoint {
+    timestamp: String! @fake(type: recentDate, options: {dateFormat: "YYYY-MM-DD"})
+    open: Float!
+    close: Float!
+    high: Float!
+    low: Float!
+    volume: Float!
+  }
+
+  type Candlestick {
+    dataPoints: [DataPoint!]!
+  }
+`;
+
+app.use("/graphql", fakerProxy(fakedSchema, url));
+
+app.listen(3000);
